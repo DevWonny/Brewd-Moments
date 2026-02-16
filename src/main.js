2e231513e78d4dddfdff4dade29c7e7c5da9948a
DevWonny <cjfdnjs1994@naver.com>
2026-02-16 21:47:45 +0900

    Font Install & Image 정리

--- src/main.js
+++ src/main.js
@@ -1,7 +1,22 @@
-import "./style.css";
+import "../src/assets/scss/styles.scss";
+// Font
+// Main Title
+import "@fontsource-variable/merriweather";
+// Sub Title
+import "@fontsource/playfair-display/400.css";
+import "@fontsource/playfair-display/700.css";
+// EN
+import "@fontsource/inter/300.css";
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
+// KR
+import "@fontsource/noto-sans-kr/400.css";
+import "@fontsource/noto-sans-kr/700.css";
+// jquery
 import $ from "jquery";
 window.$ = $;
 window.jquery = $;
 
+// JS Import
 import "./js/common";
 import "./js/scroll";
